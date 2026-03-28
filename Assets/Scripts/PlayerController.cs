467ccf4dd29f34ae089e5e87a13e990553bac969
pariyakorn saelao <pariyakorn.carrot28@gmail.com>
2026-03-29 03:00:06 +0700

    Assignment 1 - Power up animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,26 +9,29 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
+    private PowerUp powerUpScript; 
+
     private InputAction moveAction;
     private InputAction smashAction;
     private InputAction breakAction;
 
-    public bool hasPowerUp = false;
+    //public bool hasPowerUp = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        powerUpScript = GetComponent<PowerUp>(); 
+
         moveAction = InputSystem.actions.FindAction("Move");
         smashAction = InputSystem.actions.FindAction("Smash");
         breakAction = InputSystem.actions.FindAction("Break");
     }
 
-    // Update is called once per frame
     void Update()
     {
         var move = moveAction.ReadValue<Vector2>();
         rb.AddForce(move.y * speed * focalPoint.forward);
+
         if (breakAction.IsPressed())
         {
             rb.linearVelocity = Vector3.zero;
@@ -37,38 +40,37 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy") && powerUpScript.hasPowerUp)
         {
-            if (hasPowerUp == true)
-            {
-                var rb = collision.gameObject.GetComponent<Rigidbody>();
-                var dir = collision.transform.position - transform.position;
-                dir.Normalize();    
-                rb.AddForce(100 * dir, ForceMode.Impulse);
-            }
+            var enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            var dir = collision.transform.position - transform.position;
+            dir.Normalize();
+            enemyRb.AddForce(100 * dir, ForceMode.Impulse);
+
+            //if (hasPowerUp == true)
+            //{
+            //    var rb = collision.gameObject.GetComponent<Rigidbody>();
+            //    var dir = collision.transform.position - transform.position;
+            //    dir.Normalize();
+            //    rb.AddForce(100 * dir, ForceMode.Impulse);
+            //}
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("PowerUp")) ;
+        if (other.CompareTag("PowerUp"))
         {
-            hasPowerUp = true;
             Destroy(other.gameObject);
+            powerUpScript.Activate(10f);
 
-            if (countdownCoroutine != null)
-            {
-                StopCoroutine(countdownCoroutine);
-            }
-            StartCoroutine(PowerUpCountDown());
+            //hasPowerUp = true;
+
+            //if (countdownCoroutine != null)
+            //{
+            //    StopCoroutine(countdownCoroutine);
+            //}
+            //StartCoroutine(PowerUpCountDown());
         }
-    }
-
-    private Coroutine countdownCoroutine;
-
-    IEnumerator PowerUpCountDown()
-    {
-        yield return new WaitForSeconds(10f);
-        hasPowerUp = false;
     }
 }
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour
+{
+    public bool hasPowerUp; 
+    public GameObject ringIndicator;
+    public float rotationSpeed = 150f;
+    public Vector3 offset = Vector3.zero;
+
+    void Update()
+    {
+        if (ringIndicator == null || !hasPowerUp)
+        {
+            if (ringIndicator != null) ringIndicator.SetActive(false);
+            return;
+        }
+
+        ringIndicator.SetActive(true);
+        ringIndicator.transform.position = transform.position + offset;
+        ringIndicator.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+
+    public void Activate(float duration)
+    {
+        StopAllCoroutines();
+        StartCoroutine(PowerUpCountDown(duration));
+    }
+
+    IEnumerator PowerUpCountDown(float duration)
+    {
+        hasPowerUp = true;
+        yield return new WaitForSeconds(duration);
+        hasPowerUp = false;
+    }
+}
